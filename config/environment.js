de15660ca6b0c26116f8748e59a6a5ff037b8854
Per Rosendahl <per.rosendahl@sweco.se>
2016-09-14 15:21:48 +0200

    Sweco Bootstrap

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'my-new-app',
+    modulePrefix: 'sweco-bootstrap',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
