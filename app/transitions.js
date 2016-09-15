63fa55ea8c0fdd0662cb4d971a55330475e277a3
Per Rosendahl <per.rosendahl@sweco.se>
2016-09-15 09:46:08 +0200

    Fix no BS route

--- app/transitions.js
+++ app/transitions.js
@@ -1,10 +1,3 @@
 export default function(){
 
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('bootstrap'),
-    this.use('fade', { duration: 300 }),
-    this.reverse('fade',  { duration: 300 })
-  );
-
 }
