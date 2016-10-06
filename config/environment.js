67ce4254dbdfa29f2623964a59e0f8db9d248df6
Christopher Haglund <christopher.haglund@sweco.se>
2016-10-06 13:21:29 +0200

    Prepare for deploy to gh-pages

--- config/environment.js
+++ config/environment.js
@@ -19,6 +19,10 @@ module.exports = function(environment) {
     }
   };
 
+  if (environment === 'gh-pages') {
+    ENV.rootURL = '/sweco-digital-guidelines/';
+  }
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
