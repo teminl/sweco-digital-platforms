a6410fe5445fd3f2ba1928624439b759084ca90f
Christopher Haglund <christopher.haglund@sweco.se>
2016-10-17 19:42:27 +0200

    Grunt script for building dist.zip

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    shell: {
+        buildEmber: 'ember build --environment=production',
+        makeTmpDir: 'mkdir dist.tmp',
+        removeTmpDir: 'rm -rf dist.tmp'
+    },
+
+    copy: {
+      fonts: { expand: true, cwd: 'dist', src: 'fonts/*', dest: 'dist.tmp' },
+      img: { expand: true, cwd: 'dist', src: 'img/*', dest: 'dist.tmp' },
+      script: { expand: true, cwd: 'dist/assets', src: 'vendor.js', dest: 'dist.tmp/scripts' },
+      css: { expand: true, cwd: 'dist/assets', src: 'sweco-bootstrap.css', dest: 'dist.tmp/css' },
+      html: { expand: true, cwd: 'dist', src: 'starter-template.html', dest: 'dist.tmp' }
+    },
+
+    rename: {
+      script: { src: 'dist.tmp/scripts/vendor.js', dest: 'dist.tmp/scripts/sweco-bootstrap.min.js' },
+      css: { src: 'dist.tmp/css/sweco-bootstrap.css', dest: 'dist.tmp/css/sweco-bootstrap.min.css' }
+    },
+
+    compress: {
+      main: {
+        options: { archive: 'dist.zip' },
+        files: [ { src: ['dist.tmp/**'], dest: '' } ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-rename');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
+  grunt.registerTask('default', ['shell:buildEmber', 'shell:makeTmpDir', 'copy', 'rename', 'compress', 'shell:removeTmpDir']);
+
+};
