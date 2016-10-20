8bfce53554c5538b503ced51110e0868472d76a4
Per Rosendahl <per.rosendahl@sweco.se>
2016-10-20 14:27:43 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,15 @@ module.exports = function(grunt) {
     shell: {
         buildEmber: 'ember build --environment=production',
         makeTmpDir: 'mkdir sweco-bootstrap',
-        removeTmpDir: 'rm -rf sweco-bootstrap'
+        removeTmpDir: 'rm -rf sweco-bootstrap',
+        removeDevScss: 'rm -rf sweco-bootstrap/scss/_sweco-dev.scss'
     },
 
     copy: {
       fonts: { expand: true, cwd: 'dist', src: 'fonts/*', dest: 'sweco-bootstrap' },
       img: { expand: true, cwd: 'dist', src: 'img/*', dest: 'sweco-bootstrap' },
-      script: { expand: true, cwd: 'dist/assets', src: 'vendor.js', dest: 'sweco-bootstrap/scripts' },
       css: { expand: true, cwd: 'dist/assets', src: 'sweco-bootstrap.css', dest: 'sweco-bootstrap/css' },
+      script: { expand: true, cwd: 'dist/assets', src: 'vendor.js', dest: 'sweco-bootstrap/scripts' },
       scriptApp: { expand: true, cwd: 'dist/scripts', src: 'application.js', dest: 'sweco-bootstrap/scripts' },
       cssApp: { expand: true, cwd: 'dist/css', src: 'application.css', dest: 'sweco-bootstrap/css' },
       html: { expand: true, cwd: 'dist', src: 'starter-template.html', dest: 'sweco-bootstrap' }
@@ -35,6 +36,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-rename');
   grunt.loadNpmTasks('grunt-contrib-compress');
 
-  grunt.registerTask('default', ['shell:buildEmber', 'shell:makeTmpDir', 'copy', 'rename', 'compress', 'shell:removeTmpDir']);
+  grunt.registerTask('default', ['shell:buildEmber', 'shell:removeTmpDir', 'shell:makeTmpDir', 'copy', 'rename', 'shell:removeDevScss', 'compress']);
 
 };
