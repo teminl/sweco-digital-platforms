0d2731eba7d7b102abb34ae23f333cae15a296f6
Per Rosendahl <per.rosendahl@sweco.se>
2016-10-25 11:59:41 +0200

    publish: Latest build
    generated from commit f0efc365a9cbe7e0db351f1e150bb88839b52a4c

--- assets/sweco-bootstrap.js
+++ assets/sweco-bootstrap.js
@@ -1388,7 +1388,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("sweco-bootstrap/app")["default"].create({"name":"sweco-bootstrap","version":"1.0.0+1ca4f738"});
+  require("sweco-bootstrap/app")["default"].create({"name":"sweco-bootstrap","version":"1.0.0+f0efc365"});
 }
 
 /* jshint ignore:end */
